83d6183369628efcd256b3a817f53eb03e3a54fc
Manish Kumar <manish320kumar230@gmail.com>
2024-07-29 13:32:09 +0530

    Init

---
+++ src/components/Body.tsx
@@ -0,0 +1,15 @@
+
+
+const Body = () => {
+  return (
+    <div>
+        {/* <img src="https://image-1.uhdpaper.com/wallpaper/assassins-creed-jade-8k-wallpaper-uhdpaper.com-279@1@m.jpg" alt="bg"  /> */}
+        <img className="bgpic" src="https://www.bhmpics.com/downloads/assassins-creed-wallpaper/1.5ff95caf8b4bc2e99994d2a1a4a1fc48.jpg" alt="bg"  />
+        <p>
+            Assassins Creed
+        </p>
+    </div>
+  )
+}
+
+export default Body;
